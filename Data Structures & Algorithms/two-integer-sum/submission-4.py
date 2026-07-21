4ae870ec4fbb676eececb91411b07090c43fc6ed
yohannUL <145624583+yohannUL@users.noreply.github.com>
2026-07-20 22:55:42 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        vus = {}
+
+        for indice, nombre in enumerate(nums):
+            partenaire = target - nombre
+
+            if partenaire in vus:
+                return [vus[partenaire], indice]
+
+            vus[nombre] = indice
+
+
+        return [1,1]
